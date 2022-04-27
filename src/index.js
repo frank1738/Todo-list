5612e1cfac205ea6695f506ef184921463d4011d
frank1738 <98453979+frank1738@users.noreply.github.com>
2022-04-27 22:24:54 +0300

    add file

--- src/index.js
+++ src/index.js
@@ -1,31 +1,38 @@
 import './style.css';
+import { addTask } from './modules/addRemove.js';
+import { removeItem } from './modules/addRemove.js';
 
+const addData = document.querySelector('.input');
 const todoContainer = document.querySelector('.tasks');
-const todoItems = [
-  {
-    description: 'wash the dishes',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'complete To Do list project',
-    completed: false,
-    index: 2,
-  },
-];
+const submit = document.querySelector('.fa-arrow-down');
+let taskList = [];
 
-const loadTodos = () => {
-  for (let i = 0; i < todoItems.length; i += 1) {
+if (JSON.parse(localStorage.getItem('tasks')).length > 0) {
+  const localItems = JSON.parse(localStorage.getItem('tasks'));
+  taskList = localItems;
+  for (let i = 0; i < localItems.length; i += 1) {
     const taskElement = document.createElement('div');
     taskElement.classList.add('task');
-    taskElement.setAttribute('id', todoItems[i].index);
+    taskElement.setAttribute('id', localItems[i].index);
     taskElement.innerHTML = `
-          <input type="checkbox" name="" id="${todoItems[i].index}" class="check-box" />
-          <p class="description">${todoItems[i].description}</p>
-          <i class="fa-solid fa-ellipsis-vertical"></i>
-          `;
+                    <input type="checkbox" name="" id="${localItems[i].index}" class="check-box" />
+                    <input type="text" class="description">
+                    <i class="fa-solid fa-ellipsis-vertical"></i>
+                    `;
+    const taskDescription = taskElement.querySelector('.description');
+    taskDescription.value = localItems[i].description;
     todoContainer.appendChild(taskElement);
+    const edit = taskElement.querySelector('.description');
+    edit.addEventListener('click', removeItem);
   }
-};
+}
 
-loadTodos();
+localStorage.setItem('tasks', JSON.stringify(taskList));
+
+submit.addEventListener('click', addTask);
+
+addData.addEventListener('keypress', function (e) {
+  if (e.key === 'Enter') {
+    addTask();
+  }
+});
