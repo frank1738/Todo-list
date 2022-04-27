5ec2fe47b606519e10d69fbf30de3f90701531c9
frank1738 <98453979+frank1738@users.noreply.github.com>
2022-04-28 00:14:54 +0300

    implement changes

--- src/index.js
+++ src/index.js
@@ -4,29 +4,30 @@ import { addTask, removeItem } from './modules/addRemove.js';
 const addData = document.querySelector('.input');
 const todoContainer = document.querySelector('.tasks');
 const submit = document.querySelector('.fa-arrow-down');
-let taskList = [];
 
-if (JSON.parse(localStorage.getItem('tasks')).length > 0) {
-  const localItems = JSON.parse(localStorage.getItem('tasks'));
-  taskList = localItems;
-  for (let i = 0; i < localItems.length; i += 1) {
-    const taskElement = document.createElement('div');
-    taskElement.classList.add('task');
-    taskElement.setAttribute('id', localItems[i].index);
-    taskElement.innerHTML = `
+const loadTasks = () => {
+  if (!localStorage.getItem('tasks')) {
+    const taskList = [];
+    localStorage.setItem('tasks', JSON.stringify(taskList));
+  } else {
+    const localItems = JSON.parse(localStorage.getItem('tasks'));
+    for (let i = 0; i < localItems.length; i += 1) {
+      const taskElement = document.createElement('div');
+      taskElement.classList.add('task');
+      taskElement.setAttribute('id', localItems[i].index);
+      taskElement.innerHTML = `
                     <input type="checkbox" name="" id="${localItems[i].index}" class="check-box" />
                     <input type="text" class="description">
                     <i class="fa-solid fa-ellipsis-vertical"></i>
                     `;
-    const taskDescription = taskElement.querySelector('.description');
-    taskDescription.value = localItems[i].description;
-    todoContainer.appendChild(taskElement);
-    const edit = taskElement.querySelector('.description');
-    edit.addEventListener('click', removeItem);
+      const taskDescription = taskElement.querySelector('.description');
+      taskDescription.value = localItems[i].description;
+      todoContainer.appendChild(taskElement);
+      const edit = taskElement.querySelector('.description');
+      edit.addEventListener('click', removeItem);
+    }
   }
-}
-
-localStorage.setItem('tasks', JSON.stringify(taskList));
+};
 
 submit.addEventListener('click', addTask);
 
@@ -35,3 +36,4 @@ addData.addEventListener('keypress', (e) => {
     addTask();
   }
 });
+loadTasks();
