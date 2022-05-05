9480bcd6fd03cdb6bd95d54f6dd2b73ea065a9ae
frank1738 <98453979+frank1738@users.noreply.github.com>
2022-05-05 11:59:27 +0300

    add test

--- __mocks__/addRemove.js
+++ __mocks__/addRemove.js
@@ -1,9 +1,9 @@
-let localList=[]
+let localList = [];
 const addTask = () => {
   const task = {
     description: 'coding',
     completed: false,
-    index: 1,
+    index: localList.length + 1,
   };
   document.body.innerHTML = '<div>' + '  <div class="tasks"></div>' + '</div>';
   const tasks = document.querySelector('.tasks');
@@ -11,15 +11,25 @@ const addTask = () => {
   taskElement.classList.add('task');
   taskElement.setAttribute('id', task.index);
   tasks.appendChild(taskElement);
-  localList.push(task)
+  localList.push(task);
   localStorage.setItem('tasks', JSON.stringify(localList));
   const myTasks = document.querySelectorAll('.task');
   return myTasks;
 };
 
+const updateLocal = () => {
+  const div = document.querySelector('.task');
+  const key = Number(div.id);
+  const localData = localList;
+  localData[key - 1].description = 'play football';
+  const editedTask = localData[key - 1].description;
+  localStorage.setItem('tasks', JSON.stringify(localData));
+  return editedTask;
+};
+
 const deleteData = () => {
   const div = document.querySelector('.task');
-  const key=div.id
+  const key = div.id;
   const filtered = localList.filter((item) => item.index !== key);
   localStorage.setItem('tasks', JSON.stringify(filtered));
   div.remove();
@@ -27,7 +37,34 @@ const deleteData = () => {
   return divs;
 };
 
+const Mark = () => {
+  const div = document.querySelector('.task');
+  const key = div.id;
+  const localData = localList;
+  localData[key - 1].completed = true;
+  const completedStatus = localData[key - 1].completed;
+  localStorage.setItem('tasks', JSON.stringify(localData));
+  return completedStatus;
+};
+
+const deletAll = () => {
+  const div = document.querySelector('.task');
+  const key = div.id;
+  const localData = localList;
+  const filtered = localData.filter((item) => item.completed === false);
+  localStorage.setItem('tasks', JSON.stringify(filtered));
+  const completedStatus = localData[key - 1].completed;
+  if (completedStatus === true) {
+    div.remove();
+  }
+  const divs = document.querySelectorAll('.task');
+  return divs;
+};
+
 module.exports = {
+  edit: updateLocal,
   add: addTask,
   delete: deleteData,
+  complete: Mark,
+  clear: deletAll,
 };
